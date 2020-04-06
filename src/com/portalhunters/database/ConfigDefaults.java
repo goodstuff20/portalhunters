17c1a67eb2ec1f0775bc1eda5bbdb7971ba13911
Gustav Lund <gustllund@gmail.com>
2020-04-06 17:54:54 +0200

    class stuff

--- src/com/portalhunters/database/ConfigDefaults.java
+++ src/com/portalhunters/database/ConfigDefaults.java
@@ -1,7 +1,9 @@
 package com.portalhunters.database;
 
 import com.portalhunters.PortalHunters;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
 
 public class ConfigDefaults {
 
@@ -10,7 +12,10 @@ public class ConfigDefaults {
         c.addDefault(path + "baseXP", "625");
         c.addDefault(path + "exponent", "1.9");
         path = "weapons.sword.";
+        c.addDefault(path + "material", Material.IRON_SWORD);
         c.addDefault(path + "name", "&b&lSword"); //TODO weitere daten in die default config rein tun
+        c.addDefault(path + "katana.material", Material.GOLDEN_SWORD);
+        c.addDefault(path + "spear.material", Material.TRIDENT);
 
         c.options().header( //TODO shit entfernen und neu schreiben
                 "##### Portal Hunters Configuration #####\n" +
@@ -22,6 +27,8 @@ public class ConfigDefaults {
                         "You can use many variables. Here is a list:\n" +
                         "\n" +
                         "  %playing% - The current amount of players in a lobby!\n");
+
+
 
         c.options().copyDefaults(true);
         PortalHunters.saveConfigFile();
